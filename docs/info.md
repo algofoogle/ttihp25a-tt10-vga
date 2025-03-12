4b263bec9021f16e8fec6a6459df86b5e0d322d1
Anton Maurovic <anton@foogle.com>
2025-03-12 15:13:02 +1030

    First rough VGA 'game' implementation

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,18 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Typical Verilog design that generates VGA timing and RGB222 colour outputs compatible with the Tiny VGA PMOD.
+
 
 ## How to test
 
-Explain how to use your project
+*   Plug in a VGA monitor via Tiny VGA PMOD.
+*   Set `mode` input to 0 for 640x480 60Hz from a 25.175MHz clock, or to 1 for 1440x900 60Hz from a 26.6175 MHz clock.
+*   Supply your clock.
+*   Assert reset.
+
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+Tiny VGA PMOD and VGA monitor is all you should need externally.
+
--- src/project.v
+++ src/project.v
@@ -5,7 +5,9 @@
 
 `default_nettype none
 
-module tt_um_example (
+`define RGB [5:0] // RrGgBb order
+
+module tt_um_algofoogle_vga (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +18,110 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  localparam kGrassTop      = 384;
+  localparam kDirtTop       = kGrassTop + 16;
+  localparam kPlayerWidth   = 16;
+  localparam kPlayerHeight  = 16;
+  localparam kRangeX        = 640 - kPlayerWidth;
+  localparam kRangeY        = kGrassTop - kPlayerHeight;
+
+  wire reset = ~rst_n;
+  wire video_timing_mode = ui_in[7];
+  wire hsync;
+  wire vsync;
+  wire [1:0] rr,gg,bb;
+  wire [9:0] h,v;
+  wire hmax,vmax, hblank,vblank;
+  wire visible; // Whether the display is in the visible region, or blanking region.
+
+  // Tiny VGA PMOD wiring, with 'visible' used for blanking:
+  assign uo_out = {
+    hsync,
+    {3{visible}} & {bb[0], gg[0], rr[0]},
+    vsync,
+    {3{visible}} & {bb[1], gg[1], rr[1]}
+  };
+
+  assign uio_out = {
+    3'b000, // Unused.
+    visible,
+    vblank,
+    hblank,
+    vmax,
+    hmax
+  };
+  assign uio_oe  = 8'b0001_1111; // Top 3 bidir pins are inputs, rest are outputs.
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
+
+  vga_sync vga_sync(
+    .clk      (clk),
+    .reset    (reset),
+    .mode     (video_timing_mode),
+    .o_hsync  (hsync),
+    .o_vsync  (vsync),
+    .o_hpos   (h),
+    .o_vpos   (v),
+    .o_hmax   (hmax),
+    .o_vmax   (vmax),
+    .o_hblank (hblank),
+    .o_vblank (vblank),
+    .o_visible(visible)
+  );
+
+  wire `RGB sky     = 6'b01_10_11; // Light blue.
+  wire `RGB grass   = 6'b01_10_00; // Lively green.
+  wire `RGB dirt    = 6'b10_01_00; // Medium brown.
+  wire `RGB player  = 6'b10_00_00; // Red.
+
+  // Player position:
+  reg [9:0] px, py;
+  reg dx; // 0=left, 1=right
+
+  // X direction control:
+  always @(posedge clk) begin
+    if (reset) begin
+      dx <= 1;
+    end else if (px == kRangeX) begin
+      dx <= 0; // Move left.
+    end else if (px == 0) begin
+      dx <= 1; // Move right.
+    end
+  end
+
+  // X position control:
+  always @(posedge clk) begin
+    if (reset) begin
+      px <= 0;
+    end else if (dx) begin
+      px <= px + 1;
+    end else begin
+      px <= px - 1;
+    end
+  end
+
+  // Y position control:
+  always @(posedge clk) begin
+    if (reset) begin
+      py <= 0;
+    end
+  end
+
+  wire in_player =
+    (h >= px) && (h < px+kPlayerWidth) &&
+    (v >= kGrassTop-py-kPlayerHeight) && (v < kGrassTop-py);
+
+  wire in_grass = (v >= kGrassTop);
+  wire in_dirt = (v >= kDirtTop);
+
+  wire `RGB rgb =
+    in_dirt   ? dirt :
+    in_grass  ? grass :
+    in_player ? player :
+                sky;
+
+  assign {rr,gg,bb} = rgb;
 
 endmodule
+
